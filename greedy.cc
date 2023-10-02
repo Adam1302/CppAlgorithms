3fc227adc7602efb40557ea72d3fcc2c36a911c5
Adam Abdulhamid <adam13132002@hotmail.com>
2023-10-02 11:54:46 -0400

    Minimum Interval Colouring Added

--- greedy.cc
+++ greedy.cc
@@ -6,6 +6,7 @@
 #endif
 
 #include <iostream>
+#include <queue>
 #include <vector>
 
 template <typename T>
@@ -27,7 +28,7 @@ std::ostream& operator<<(std::ostream& out, std::vector<T> vec) {
 
 template <typename T>
 std::vector<std::pair<T, T>> maxDisjointIntervalScheduling(
-    std::vector<std::pair<T, T>>& intervals
+    std::vector<std::pair<T, T>> intervals
 ) {
     std::vector<std::pair<T, T>> disjoints;
     std::sort(
@@ -50,13 +51,50 @@ std::vector<std::pair<T, T>> maxDisjointIntervalScheduling(
     return disjoints;
 }
 
+template <typename T>
+int minColoursNeeded(
+    std::vector<std::pair<T, T>> intervals
+) {
+    std::sort(
+        intervals.begin(),
+        intervals.end(),
+        [](auto& elem1, auto& elem2) {
+            return elem1.first < elem2.first;
+        }
+    );
+    
+    std::priority_queue<int, std::vector<int>, std::greater<int>> finishTimes;
+    size_t maxCount = 0;
+
+    for (int i = 0; i < intervals.size(); ++i) {
+        T current = intervals[i].first;
+        while (!finishTimes.empty() && current > finishTimes.top()) {
+            finishTimes.pop();
+        }
+        finishTimes.push(intervals[i].second);
+        if (finishTimes.size() > maxCount)
+            maxCount = finishTimes.size();
+    }
+    return maxCount;
+}
+
 int main() {
     LOG("Hello World");
 
     std::vector<std::pair<int, int>> vec1{
         {2,8}, {3,4}, {5,6}
     };
+    std::vector<std::pair<int, int>> vec2{
+        {2,4}, {3,5}, {4,6}, {5,7}, {6,8}
+    };
+    std::vector<std::pair<int, int>> vec3{
+        {2,4}, {3,6}, {1,6}, {6,7}, {5,8}
+    };
     std::cout << maxDisjointIntervalScheduling(vec1) << "\n";
+    std::cout << maxDisjointIntervalScheduling(vec2) << "\n";
+
+    std::cout << minColoursNeeded(vec2) << "\n";
+    std::cout << minColoursNeeded(vec3) << "\n";
     // sorting::mergesort(vec1);
     // std::cout << vec1 << "\n";
 }
